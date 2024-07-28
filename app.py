b60a95c4109c6cc629c50b8ed9912d184bc9218d
Vansh Shah <shah.vansh132@gmail.com>
2024-07-29 01:48:33 +0530

    vercel file added

--- app.py
+++ app.py
@@ -40,7 +40,3 @@ async def detect_text(
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
